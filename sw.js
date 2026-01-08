9c19a11d73f44db81e30a8869c64edcca3e23ce5
goldduotw <151952616+goldduotw@users.noreply.github.com>
2026-01-07 20:26:19 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,5 +4,10 @@ self.addEventListener('install', (event) => {
 
 // CRITICAL: Chrome needs to see a 'fetch' listener to enable the Install button
 self.addEventListener('fetch', (event) => {
+// If the URL has Epic's login code, don't use the cache!
+  if (event.request.url.includes('code=')) {
+    return fetch(event.request);
+  }
+  // Otherwise, do the normal stuff
   event.respondWith(fetch(event.request));
 });
